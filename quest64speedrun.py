aab3ca5292ebe80de17db1a338828dd2c00d4025
“rwp005” <rwp005@gmail.com>
2023-12-27 15:53:44 -0600

    first commit

---
+++ quest64speedrun.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+    
+st.write("""
+# Quest 64 Speed Run
+Any% Glitchless 2:34
+""")
+    
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
